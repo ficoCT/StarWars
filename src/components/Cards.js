0fcb4f606c9927df81cb423a4d404d3fdba99df9
ficoCT <fico.dev.ct@gmail.com>
2022-11-02 11:30:35 +0100

    Paginate.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,83 +1,96 @@
 import * as React from 'react';
 import {useEffect, useState} from "react";
+import ReactPaginate from 'react-paginate';
+import Card from './Card';
 
-export default function Cards({}) {
+export default function MyFavorite() {
 
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [url, setUrl] = useState('https://swapi.dev/api/people');
     const [people, setPeople] = useState([]);
-    const [films, setFilms] = useState([]);
-    const [planets, setPlanets] = useState([]);
-    const [starships, setStarships] = useState([]);
-    const [vehicles, setVehicles] = useState([]);
+    const [pageCount, setPageCount] = useState(5);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
 
-    const downloadData = async() => {
-
-        const urls = [
-            "https://swapi.dev/api/people",
-            "https://swapi.dev/api/films",
-            "https://swapi.dev/api/planets",
-            "https://swapi.dev/api/starships",
-            "https://swapi.dev/api/vehicles"
-        ];
-
-        const data = {}
-
-        Promise.all(urls.map(url =>
-            fetch(url)
-                .then(checkStatus)
-                .then(parseJSON)
-                .catch(error => setError(error))
-        ))
-            .then(apiData => {
-        
-                data.people = apiData[0].results;
-                data.films = apiData[1].results;
-                data.planets = apiData[2].results;
-                data.starships = apiData[3].results;
-                data.vehicles = apiData[4].results;
-
+    const handleFetch = () => {
+        console.log('handleFetch');
+        fetch(url)
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(body => {
+                console.log('body', body);
+                setUrl(body.next);
+                setPeople(body.results)
+                setIsLoaded(true);
+                setPageCount(Math.ceil(body.count/10));
             })
-
-        function checkStatus(response) {
-            if (response.ok) {
-                return Promise.resolve(response);
-            } else {
-                return Promise.reject(new Error(response.statusText));
-            }
-        }
-
-        function parseJSON(response) {
-            return response.json();
-        }
-
-        return data;
-
+            .catch(error => setError(error))
     }
+
+    function checkStatus(response) {
+        if (response.ok) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(new Error(response.statusText));
+        }
+    }
+
+    function parseJSON(response) {
+        return response.json();
+    }
+
+    const handlePageChange = (selectedObject) => {
+        let u = "";
+        if (selectedObject===1) { u = "https://swapi.dev/api/people"}
+        else {
+            u = `https://swapi.dev/api/people/?page=${selectedObject.selected}`
+        }
+        setUrl(u);
+        handleFetch();
+    };
 
     useEffect(() => {
 
-        downloadData().then(data => {
-
-            setIsLoaded(true);
-            setPeople(data.people);
-            setFilms(data.results);
-            setPlanets(data.results);
-            setStarships(data.results);
-            setVehicles(data.results);
-
-        });
+        handleFetch();
 
     }, [])
 
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-        return <div>Jeszcze trochę i zaraz się załaduje ... :)</div>;
+        return <div>Karty zaraz będą :) ...</div>;
     } else {
         return (
-                <h1>Cards</h1>
+            <>
+                <br />
+                <br />
+                <ul>
+                    {
+                        people.length === 0 ?
+                            "Karty zaraz będą :)"
+                            :
+                            people.map(card => (
+                                <li key={card.name}>
+                                    <Card card={card}/>
+                                </li>
+                            ))}
+                </ul>
+                <br />
+                <br />
+                <ReactPaginate
+                    pageCount={pageCount}
+                    pageRange={2}
+                    marginPagesDisplayed={2}
+                    onPageChange={handlePageChange}
+                    containerClassName={'container'}
+                    previousLinkClassName={'page'}
+                    breakClassName={'page'}
+                    nextLinkClassName={'page'}
+                    pageClassName={'page'}
+                    disabledClassNae={'disabled'}
+                    activeClassName={'active'}
+                />
+            </>
+
         );
     }
-
 }
